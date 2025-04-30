efc238d0d45e044c8346dcb3ebcb733d69f1551c
Joseph <underradicals@gmail.com>
2025-04-30 14:08:38 -0500

    Test Pipeline

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, UserConfig } from "vite";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+
+} satisfies UserConfig);
